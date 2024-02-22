435f96e8a7b3aec12850dea8797bdddbffd965eb
Rishala Itshnyk <rishala.itshnyk@gmail.com>
2024-02-22 17:05:08 +0200

    initial commit

---
+++ Laboratorna_7_1.Tests/Tests.cs
@@ -0,0 +1,133 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Laboratorna_7_1.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestCreate()
+        {
+            const int rowCount = 7;
+            const int colCount = 9;
+            int[][] r = new int[rowCount][];
+            for (int i = 0; i < rowCount; ++i)
+                r[i] = new int[colCount]; 
+
+            Program.Create(r, rowCount, colCount, -12, 37);
+
+            for (int i = 0; i < rowCount; ++i)
+            for (int j = 0; j < colCount; ++j)
+                Assert.IsTrue(r[i][j] >= -12 && r[i][j] <= 37);
+        }
+
+        [Test]
+        public void TestSort()
+        {
+            const int rowCount = 7;
+            const int colCount = 9;
+            int[][] r = new int[rowCount][];
+            for (int i = 0; i < rowCount; ++i)
+                r[i] = new int[colCount];
+
+            int[][] testArray = new int[][]
+            {
+                new int[] {5, 3, 4, 2, 1, 6, 8, 7, 9},
+                new int[] {1, 2, 3, 4, 5, 6, 8, 7, 9},
+                new int[] {6, 5, 4, 3, 2, 1, 8, 7, 9},
+                new int[] {4, 3, 6, 5, 1, 2, 8, 7, 9},
+                new int[] {1, 2, 3, 4, 5, 6, 8, 7, 9},
+                new int[] {6, 5, 4, 3, 2, 1, 8, 7, 9},
+                new int[] {5, 3, 4, 2, 1, 6, 8, 7, 9},
+            };
+
+            for (int i = 0; i < rowCount; ++i)
+                r[i] = (int[])testArray[i].Clone();
+
+            Program.Sort(r, rowCount, colCount);
+
+            for (int i = 0; i < rowCount - 1; ++i)
+            {
+                bool condition =
+                    (r[i][0] < r[i + 1][0]) ||
+                    (r[i][0] == r[i + 1][0] && r[i][1] < r[i + 1][1]) ||
+                    (r[i][0] == r[i + 1][0] && r[i][1] == r[i + 1][1] && r[i][3] >= r[i + 1][3]);
+
+                Assert.IsTrue(condition, $"Failed at index {i}");
+            }
+        }
+
+        [Test]
+        public void TestPrint()
+        {
+            const int rowCount = 7;
+            const int colCount = 9;
+            int[][] r = new int[rowCount][];
+            for (int i = 0; i < rowCount; ++i)
+                r[i] = new int[colCount];
+
+            int[][] testArray = new int[][]
+            {
+                new int[] {1, 2, 3, 4, 5, 6, 8, 7, 9},
+                new int[] {7, 8, 9, 10, 11, 12, 8, 7, 9},
+                new int[] {13, 14, 15, 16, 17, 18, 8, 7, 9},
+                new int[] {19, 20, 21, 22, 23, 24, 8, 7, 9},
+                new int[] {25, 26, 27, 28, 29, 30, 8, 7, 9},
+                new int[] {31, 32, 33, 34, 35, 36, 8, 7, 9},
+                new int[] {37, 38, 39, 40, 41, 42, 8, 7, 9},
+            };
+
+            for (int i = 0; i < rowCount; ++i)
+                r[i] = (int[])testArray[i].Clone();
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Program.Print(r, rowCount, colCount);
+                Console.SetOut(Console.Out);
+
+                string printedOutput = sw.ToString();
+                string expectedOutput = "\n   1   2   3   4   5   6   8   7   9\n"
+                                        + "   7   8   9  10  11  12   8   7   9\n"
+                                        + "  13  14  15  16  17  18   8   7   9\n"
+                                        + "  19  20  21  22  23  24   8   7   9\n"
+                                        + "  25  26  27  28  29  30   8   7   9\n"
+                                        + "  31  32  33  34  35  36   8   7   9\n"
+                                        + "  37  38  39  40  41  42   8   7   9\n\n";
+                Assert.AreEqual(expectedOutput, printedOutput);
+            }
+        }
+
+        [Test]
+        public void TestCalc()
+        {
+            const int rowCount = 7;
+            const int colCount = 9;
+            int[][] r = new int[rowCount][];
+            for (int i = 0; i < rowCount; ++i)
+                r[i] = new int[colCount];
+
+            int[][] testArray = new int[][]
+            {
+                new int[] {1, 2, 3, 4, 5, 6, 8, 7, 9},
+                new int[] {-1, -2, -3, -4, -5, -6, 8, 7, 9},
+                new int[] {0, 0, 0, 0, 0, 0, 8, 7, 9},
+                new int[] {1, -1, 2, -2, 3, -3, 8, 7, 9},
+                new int[] {2, 4, 6, 8, 10, 12, 8, 7, 9},
+                new int[] {-2, -4, -6, -8, -10, -12, 8, 7, 9},
+                new int[] {1, 3, 5, 7, 9, 11, 8, 7, 9},
+            };
+
+            for (int i = 0; i < rowCount; ++i)
+                r[i] = (int[])testArray[i].Clone();
+
+            int S = 0, k = 0;
+            Program.Calc(r, rowCount, colCount, ref S, ref k);
+
+            Assert.AreEqual(S, 168);
+            Assert.AreEqual(k, 28);
+        }
+    }
+}
---
+++ Laboratorna_7_1/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Laboratorna_7_1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int Low = -12;
+            int High = 37;
+            int rowCount = 7;
+            int colCount = 9;
+
+            int[][] r = new int[rowCount][];
+            for (int i = 0; i < rowCount; i++)
+                r[i] = new int[colCount];
+
+            Create(r, rowCount, colCount, Low, High);
+            Print(r, rowCount, colCount);
+            Sort(r, rowCount, colCount);
+            Print(r, rowCount, colCount);
+
+            int S = 0;
+            int k = 0;
+
+            Calc(r, rowCount, colCount, ref S, ref k);
+            Console.WriteLine($"S = {S}");
+            Console.WriteLine($"k = {k}");
+
+            Print(r, rowCount, colCount);
+
+            for (int i = 0; i < rowCount; i++)
+                Array.Clear(r[i], 0, colCount);
+        }
+
+        public static void Create(int[][] r, int rowCount, int colCount, int Low, int High)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < rowCount; i++)
+            for (int j = 0; j < colCount; j++)
+                r[i][j] = Low + rand.Next(High - Low + 1);
+        }
+
+        public static void Print(int[][] r, int rowCount, int colCount)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++)
+                    Console.Write($"{r[i][j],4}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void Change(int[][] a, int row1, int row2, int colCount)
+        {
+            int tmp;
+            for (int j = 0; j < colCount; j++)
+            {
+                tmp = a[row1][j];
+                a[row1][j] = a[row2][j];
+                a[row2][j] = tmp;
+            }
+        }
+
+        public static void Sort(int[][] r, int rowCount, int colCount)
+        {
+            for (int i0 = 0; i0 < rowCount - 1; i0++)
+            {
+                for (int i1 = 0; i1 < rowCount - i0 - 1; i1++)
+                {
+                    if ((r[i1][0] > r[i1 + 1][0]) ||
+                        (r[i1][0] == r[i1 + 1][0] && r[i1][1] > r[i1 + 1][1]) ||
+                        (r[i1][0] == r[i1 + 1][0] && r[i1][1] == r[i1 + 1][1] && r[i1][3] < r[i1 + 1][3]))
+                    {
+                        Change(r, i1, i1 + 1, colCount);
+                    }
+                }
+            }
+        }
+
+        public static void Calc(int[][] r, int rowCount, int colCount, ref int S, ref int k)
+        {
+            S = 0;
+            k = 0;
+            for (int i = 0; i < rowCount; i++)
+            for (int j = 0; j < colCount; j++)
+                if (r[i][j] > 0 && r[i][j] % 3 != 0)
+                {
+                    S += r[i][j];
+                    k++;
+                    r[i][j] = 0;
+                }
+        }
+    }
+}
